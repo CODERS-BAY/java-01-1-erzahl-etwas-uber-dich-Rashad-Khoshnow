3de3aa55c45bb6442180d6780adc87e6787e4b31
Rashad <rashad.khoshnow@gmail.com>
2020-08-27 09:47:17 +0200

    Add data types

---
+++ src/Aufgabe/Main.java
@@ -0,0 +1,17 @@
+package Aufgabe;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int age = 26;
+        String name = "Rashad";
+        char gender = 'm';
+        String familyName = "Khoshnow";
+        String birthDate = "21.01.1994";
+        double pointAverage = 3.0;
+        boolean married = false;
+        System.out.println("age: " + age + " |name: " + name + " |gender: " + gender + " |family name: " + familyName + " |birth date: " + birthDate + " |point average: " + pointAverage + " |married : " + married);
+    }
+}
