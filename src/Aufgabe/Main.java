a340ed0f19fefb6cd255fc76aa85e84c414d3026
Rashad <rashad.khoshnow@gmail.com>
2020-08-27 11:26:24 +0200

    LocalDate hinzugefügt

--- src/Aufgabe/Main.java
+++ src/Aufgabe/Main.java
@@ -1,17 +1,18 @@
 package Aufgabe;
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
+        LocalDate birthDate = LocalDate.of(1994,01,21);
         int age = 26;
         String name = "Rashad";
         char gender = 'm';
         String familyName = "Khoshnow";
-        String birthDate = "21.01.1994";
         double pointAverage = 3.0;
         boolean married = false;
-        System.out.println("age: " + age + " |name: " + name + " |gender: " + gender + " |family name: " + familyName + " |birth date: " + birthDate + " |point average: " + pointAverage + " |married : " + married);
+        System.out.printf(" age: " + age + " |name: " + name + " |gender: " + gender + " |family name: " + familyName + " |birth date: " + birthDate + " |point average: " + pointAverage + " |married : " + married);
     }
 }
